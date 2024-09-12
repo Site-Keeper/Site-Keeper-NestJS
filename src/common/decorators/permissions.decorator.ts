808e3889fa9b3e575bdee9379ef4c58501aa2be7
18zuluaga <18zuluaga1989@gmail.com>
2024-09-12 16:08:41 -0500

    activate swagger auth

--- src/common/decorators/permissions.decorator.ts
+++ src/common/decorators/permissions.decorator.ts
@@ -1,11 +1,9 @@
 import { applyDecorators, SetMetadata, UseGuards } from '@nestjs/common';
-// import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth } from '@nestjs/swagger';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
 
 export function PrivateService() {
-  return applyDecorators(
-    UseGuards(AuthGuard) /* , ApiBearerAuth('access-token') */
-  );
+  return applyDecorators(UseGuards(AuthGuard), ApiBearerAuth('access-token'));
 }
 
 export const Permissions = (...permissions: string[]) =>
