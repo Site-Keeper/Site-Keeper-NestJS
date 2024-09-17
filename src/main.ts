628bfcac4177dff80d5a2cc98c7dc468466a19e7
18zuluaga <18zuluaga1989@gmail.com>
2024-09-16 20:42:47 -0500

    add cors configuration

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,12 @@ async function bootstrap() {
       transform: true,
     })
   );
+
+  const corsOptions: CorsOptions = {
+    origin: 'localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
 
   const config = new DocumentBuilder()
     .setTitle('SiteKeeper')
@@ -29,6 +36,7 @@ async function bootstrap() {
       'access-token'
     )
     .build();
+  app.enableCors(corsOptions);
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
