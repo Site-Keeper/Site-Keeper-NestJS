18c58b5175581dbaeaf0f5d6475acef0f45357f5
Daniel <sanchitos1065@gmail.com>
2024-09-25 19:37:43 -0500

    refactor

--- src/routine/routine.service.ts
+++ src/routine/routine.service.ts
@@ -28,85 +28,67 @@ export class RoutineService {
   ) {}
 
   async create(createRoutineDto: CreateRoutineDto, userReq: UserJWT) {
-    try {
-      const user = await this.userRepository.findOne({
-        where: { id: createRoutineDto.assigned_to, is_deleted: false },
+    const user = await this.userRepository.findOne({
+      where: { id: createRoutineDto.assigned_to, is_deleted: false },
+    });
+    const newRoutine = {
+      name: createRoutineDto.name,
+      start_time: createRoutineDto.start_time,
+      end_time: createRoutineDto.end_time,
+      days: createRoutineDto.days,
+      assigned_to: user,
+      is_deleted: false,
+      created_by: userReq.id,
+      updated_by: userReq.id,
+    };
+    const routinesByUser = await this.routineRepository.find({
+      where: { is_deleted: false, assigned_to: { id: user.id } },
+    });
+    routinesByUser.forEach((routineDays) => {
+      const days = routineDays.days;
+      days.forEach((day) => {
+        if (createRoutineDto.days.includes(day)) {
+          throw new BadRequestException(
+            'The user already has a routine scheduled for this day'
+          );
+        }
       });
-      const newRoutine = {
-        name: createRoutineDto.name,
-        start_time: createRoutineDto.start_time,
-        end_time: createRoutineDto.end_time,
-        days: createRoutineDto.days,
-        assigned_to: user,
-        is_deleted: false,
-        created_by: userReq.id,
-        updated_by: userReq.id,
-      };
-      const routinesByUser = await this.routineRepository.find({
-        where: { is_deleted: false, assigned_to: { id: user.id } },
-      });
-      routinesByUser.forEach((routineDays) => {
-        const days = routineDays.days;
-        days.forEach((day) => {
-          if (createRoutineDto.days.includes(day)) {
-            throw new BadRequestException(
-              'The user already has a routine scheduled for this day'
-            );
-          }
-        });
-      });
-      await this.routineRepository.save(newRoutine);
-      delete newRoutine.assigned_to;
-      return {
-        statusCode: 201,
-        message: 'routine created successfully',
-        data: {
-          responseRoutine: { ...newRoutine, assigned_to: user.id },
-        },
-      };
-    } catch (error) {
-      console.error('Error creating the routine:', error);
-      return {
-        statusCode: 500,
-        message: 'Error creating the routine',
-        error,
-      };
-    }
+    });
+    await this.routineRepository.save(newRoutine);
+    delete newRoutine.assigned_to;
+    return {
+      statusCode: 201,
+      message: 'routine created successfully',
+      data: {
+        responseRoutine: { ...newRoutine, assigned_to: user.id },
+      },
+    };
   }
 
   async findAll() {
-    try {
-      const routines = await this.routineRepository.find({
-        where: { is_deleted: false },
-        relations: { assigned_to: true, tasks: true },
-      });
-      const routineRespos = routines.map((routine) => {
-        const name = routine.assigned_to.name;
-        delete routine.assigned_to;
-        return { ...routine, assigned_to: name };
-      });
-      return routineRespos;
-    } catch (error) {
-      console.error(error);
-      return error;
-    }
+    const routines = await this.routineRepository.find({
+      where: { is_deleted: false },
+      relations: { assigned_to: true, tasks: true },
+    });
+    const routineRespos = routines.map((routine) => {
+      const name = routine.assigned_to.name;
+      delete routine.assigned_to;
+      return { ...routine, assigned_to: name };
+    });
+    return routineRespos;
   }
 
   async findByUser(id: number) {
-    try {
-      const routines = await this.routineRepository.find({
-        where: { is_deleted: false, assigned_to: { id } },
-        relations: ['assigned_to', 'tasks'],
-      });
-      const routineRespos = routines.map((routine) => {
-        const name = routine.assigned_to.name;
-        delete routine.assigned_to;
-        return { ...routine, assigned_to: name };
-      });
-      return routineRespos;
-    } catch (error) {
-      return error;
-    }
+    const routines = await this.routineRepository.find({
+      where: { is_deleted: false, assigned_to: { id } },
+      relations: ['assigned_to', 'tasks'],
+    });
+    const routineRespos = routines.map((routine) => {
+      const name = routine.assigned_to.name;
+      delete routine.assigned_to;
+      return { ...routine, assigned_to: name };
+    });
+    return routineRespos;
   }
 
   async findRoutinesForToday(id: number, token: string) {
@@ -117,64 +99,53 @@ export class RoutineService {
       },
     };
 
-    try {
-      const today = new Intl.DateTimeFormat('en-US', {
-        weekday: 'long',
-      }).format(new Date());
+    const today = new Intl.DateTimeFormat('en-US', {
+      weekday: 'long',
+    }).format(new Date());
 
-      const routines = await this.routineRepository.find({
-        where: { is_deleted: false, assigned_to: { id } },
-        relations: ['assigned_to', 'tasks'],
-      });
+    const routines = await this.routineRepository.find({
+      where: { is_deleted: false, assigned_to: { id } },
+      relations: ['assigned_to', 'tasks'],
+    });
 
-      const todayRoutines = routines.find((routine) =>
-        routine.days.includes(today)
-      );
+    const todayRoutines = routines.find((routine) =>
+      routine.days.includes(today)
+    );
 
-      if (!todayRoutines) {
-        return { todayRoutines: null };
-      }
-
-      const spacesResponse = await axios.get(`${this.javaServiceUrl}`, config);
-      const spacesMap = spacesResponse.data.reduce(
-        (acc: Record<number, ISpace>, space: ISpace) => {
-          acc[space.id] = {
-            id: space.id,
-            name: space.name,
-            location: space.location,
-          };
-          return acc;
-        },
-        {}
-      );
-
-      const tasksWithSpaces = todayRoutines.tasks.map((task) => {
-        const space = spacesMap[task.space_id];
-        return {
-          ...task,
-          space: space || null, // Añadimos el espacio si existe
-        };
-      });
-
-      return { todayRoutines: { ...todayRoutines, tasks: tasksWithSpaces } };
-    } catch (error) {
-      console.error('Error al obtener rutinas para hoy:', error);
-      throw new InternalServerErrorException(
-        'Error al obtener rutinas para hoy'
-      );
+    if (!todayRoutines) {
+      return { todayRoutines: null };
     }
+
+    const spacesResponse = await axios.get(`${this.javaServiceUrl}`, config);
+    const spacesMap = spacesResponse.data.reduce(
+      (acc: Record<number, ISpace>, space: ISpace) => {
+        acc[space.id] = {
+          id: space.id,
+          name: space.name,
+          location: space.location,
+        };
+        return acc;
+      },
+      {}
+    );
+
+    const tasksWithSpaces = todayRoutines.tasks.map((task) => {
+      const space = spacesMap[task.space_id];
+      return {
+        ...task,
+        space: space || null,
+      };
+    });
+
+    return { todayRoutines: { ...todayRoutines, tasks: tasksWithSpaces } };
   }
 
   async findOne(id: number) {
-    try {
-      const routine = await this.routineRepository.findOne({
-        where: { id, is_deleted: false },
-        relations: ['assigned_to'],
-      });
-      return routine;
-    } catch (error) {
-      return error;
-    }
+    const routine = await this.routineRepository.findOne({
+      where: { id, is_deleted: false },
+      relations: ['assigned_to'],
+    });
+    return routine;
   }
 
   async update(id: number, updateRoutineDto: UpdateRoutineDto, user: UserJWT) {
@@ -185,34 +156,23 @@ export class RoutineService {
   }
 
   async remove(id: number, user: UserJWT, token: string) {
-    try {
-      await this.routineRepository.update(id, {
-        updated_by: user.id,
-        is_deleted: true,
-      });
+    await this.routineRepository.update(id, {
+      updated_by: user.id,
+      is_deleted: true,
+    });
 
-      const task = await this.taskService.findByRoutine(id, token);
-      console.log(task);
-      task.map((task) => {
-        return this.taskService.remove(task.id, user);
-      });
-      return;
-    } catch (error) {
-      console.error('Error update the routine:', error);
-      throw new InternalServerErrorException('Error updating the routine');
-    }
+    const task = await this.taskService.findByRoutine(id, token);
+    task.map((task) => {
+      return this.taskService.remove(task.id, user);
+    });
+    return;
   }
 
   async restore(id: number, user: UserJWT) {
-    try {
-      await this.routineRepository.update(id, {
-        updated_by: user.id,
-        is_deleted: false,
-      });
-      return;
-    } catch (error) {
-      console.error('Error update the routine:', error);
-      throw new InternalServerErrorException('Error update the routine');
-    }
+    await this.routineRepository.update(id, {
+      updated_by: user.id,
+      is_deleted: false,
+    });
+    return;
   }
 }
